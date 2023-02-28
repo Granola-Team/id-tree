67d42de1f97e3feb2dac2d027d3ec8f6cdf6dce0
dalance <dalance@gmail.com>
2023-02-28 18:53:40 +0900

    Change snowflake implementation to idgenerator

--- src/tree.rs
+++ src/tree.rs
@@ -1,7 +1,6 @@
 use std::cmp::Ordering;
 use std::collections::VecDeque;
 
-use super::snowflake::ProcessUniqueId;
 use super::*;
 
 ///
@@ -120,7 +119,7 @@ impl<T> TreeBuilder<T> {
     /// ```
     ///
     pub fn build(mut self) -> Tree<T> {
-        let tree_id = ProcessUniqueId::new();
+        let tree_id = idgenerator::IdInstance::next_id();
 
         let mut tree = Tree {
             id: tree_id,
@@ -157,7 +156,7 @@ impl<T> TreeBuilder<T> {
 #[derive(Debug)]
 #[cfg_attr(feature = "serde_support", derive(Serialize, Deserialize))]
 pub struct Tree<T> {
-    id: ProcessUniqueId,
+    id: i64,
     root: Option<NodeId>,
     pub(crate) nodes: Vec<Option<Node<T>>>,
     free_ids: Vec<NodeId>,
@@ -1765,7 +1764,7 @@ where
     T: Clone,
 {
     fn clone(&self) -> Self {
-        let tree_id = ProcessUniqueId::new();
+        let tree_id = idgenerator::IdInstance::next_id();
 
         Tree {
             id: tree_id,
